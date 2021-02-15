cc45be7463aad69a57a320eef6e432fee5a55562
Rena Jamaleddine <rena.jd12@gmail.com>
2021-02-14 20:31:43 -0500

    ajout des images

--- src/Signet.jsx
+++ src/Signet.jsx
@@ -3,7 +3,7 @@ import './Signet.scss';
 export default function Signet(props) {
     return (
         <li className="Signet">
-            <img src={'image-produits/' + props.id + '.jpeg'} alt=""/>
+            <img src={'images-signets/' + props.id + '.jpg'} alt=""/>
             <div className="titreSignet">{props.titre}</div>
             <div className="dateModification">{props.ddm}</div>
         </li>
