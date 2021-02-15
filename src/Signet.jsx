9dde04341306eb45b6841dc02d437f32d5fb81b5
Rena Jamaleddine <rena.jd12@gmail.com>
2021-02-15 08:17:34 -0500

    ajout bouttons pour chaque signet

--- src/Signet.jsx
+++ src/Signet.jsx
@@ -3,9 +3,11 @@ import './Signet.scss';
 export default function Signet(props) {
     return (
         <li className="Signet">
+            <button>Modifier</button>
             <img src={'images-signets/' + props.id + '.jpg'} alt=""/>
             <div className="titreSignet">{props.titre}</div>
             <div className="dateModification">{props.ddm}</div>
+            <button>réarranger</button>
         </li>
     );
 }
